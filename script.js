04e5794f589adbecb17fec1fd91637bcbd22646e
Tosha Shigenina <shigenina@gmail.com>
2020-01-23 23:50:40 +0500

    Добавлен комментарий

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let money, income, addExpenses, deposit, mission, period;
 
 alert('Первый урок на курсе JavaScript 11.0!');
+// после закрытия алерта происходит ошибка 
+// Unchecked runtime.lastError: The message port closed before a response was received.
 console.log('Первый урок на курсе JavaScript 11.0!');
