1be9cab4279d35efb1f53ae44517bc079d5d7e77
Tosha Shigenina <shigenina@gmail.com>
2020-01-24 22:21:38 +0500

    Вывод переменных в консоль

--- lesson02/script.js
+++ lesson02/script.js
@@ -1,6 +1,6 @@
 let money = 50000,
   income = 'Спекуляции',
-  addExpenses = 'океанариум, такси, косметика, котики',
+  addExpenses = 'Океанариум, Косметика, Котики',
   deposit = true,
   mission = 1000000,
   period = 12;
@@ -9,3 +9,11 @@ let money = 50000,
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
+
+console.log(addExpenses.length);
+
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель - заработать ' + mission + ' рублей!');
+
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
