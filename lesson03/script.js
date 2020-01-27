d43888997327ee0cd70798d17e9f81c7e0c8704b
Tosha Shigenina <shigenina@gmail.com>
2020-01-28 01:44:57 +0500

    Изменены условия в if

--- lesson03/script.js
+++ lesson03/script.js
@@ -45,18 +45,12 @@ console.log('Цель будет достигнута за: ' + Math.ceil(missio
 budgetDay = Math.floor(budgetMonth / 30);
 console.log('Бюджет на день: ' + budgetDay);
 
-if (budgetDay < 0) {
-  console.log('Что то пошло не так');
-} else if (budgetDay > 1200) {
+if (budgetDay >= 1200) {
   console.log('У вас высокий уровень дохода');
-} else if (budgetDay > 600 && budgetDay < 1200) {
+} else if (budgetDay >= 600 && budgetDay < 1200) {
   console.log('У вас средний уровень дохода');
-} else if (budgetDay < 600 && budgetDay > 0) {
+} else if (budgetDay >= 0 && budgetDay < 600) {
   console.log('К сожалению у вас уровень дохода ниже среднего');
-} else if (budgetDay === 1200) {
-  console.log('Ваш уровень дохода между средним и высоким');
-} else if (budgetDay === 600) {
-  console.log('Ваш уровень дохода между низким и средним');
-} else if (budgetDay === 0) {
-  console.log('Вы не зарабатываете');
+} else {
+  console.log('Что то пошло не так!');
 }
