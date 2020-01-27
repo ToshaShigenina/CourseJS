cd1969bc7d3f9d5c6c05e19b6a1aa089a23f367a
Tosha Shigenina <shigenina@gmail.com>
2020-01-27 22:41:11 +0500

    Добавлены условия для определения уровня дохода

--- lesson03/script.js
+++ lesson03/script.js
@@ -32,7 +32,7 @@ for (let i = 0; i < 2; i++) {
   amount[i] = prompt('Во сколько это обойдется?');
 }
 
-budgetMonth = Number(money) - amount[0] - amount[1];
+budgetMonth = money - amount[0] - amount[1];
 
 console.log('Бюджет на месяц ' + budgetMonth);
 
@@ -40,3 +40,15 @@ console.log('Цель будет достигнута за: ' + Math.ceil(missin
 
 budgetDay = Math.floor(budgetMonth / 30);
 console.log('Бюджет на день:', budgetDay);
+
+if (budgetDay < 0) {
+  console.log('Что то пошло не так');
+} else if (budgetDay > 1200) {
+  console.log('У вас высокий уровень дохода');
+} else if (budgetDay > 600 && budgetDay <= 1200) {
+  console.log('У вас средний уровень дохода');
+} else if (budgetDay <= 600) {
+  console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay === 0) {
+  console.log('Вы не зарабатываете');
+}
