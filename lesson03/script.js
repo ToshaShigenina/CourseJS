1aaa412641bbd4eba079f92fc11da98978f99aca
Tosha Shigenina <shigenina@gmail.com>
2020-01-28 01:20:06 +0500

    Созданы отдельные переменные вместо массивов, цикл удален, отдельно обработаны 600 и 1200

--- lesson03/script.js
+++ lesson03/script.js
@@ -5,8 +5,10 @@ let money = 60000,
   mission = 1000000,
   period = 12,
   budgetDay = money / 30,
-  expenses = [],
-  amount = [],
+  expenses1 = '',
+  expenses2 = '',
+  amount1 = '',
+  amount2 = '',
   budgetMonth = 0;
 
 
@@ -27,12 +29,14 @@ money = prompt('Ваш месячный доход?', '30000');
 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
 deposit = confirm('Есть ли у вас депозит в банке?');
 
-for (let i = 0; i < 2; i++) {
-  expenses[i] = prompt('Введите обязательную статью расходов?');
-  amount[i] = prompt('Во сколько это обойдется?');
-}
 
-budgetMonth = money - amount[0] - amount[1];
+expenses1 = prompt('Введите обязательную статью расходов?');
+amount1 = prompt('Во сколько это обойдется?');
+expenses2 = prompt('Введите обязательную статью расходов?');
+amount2 = prompt('Во сколько это обойдется?');
+
+
+budgetMonth = money - amount1 - amount2;
 
 console.log('Бюджет на месяц ' + budgetMonth);
 
@@ -45,10 +49,14 @@ if (budgetDay < 0) {
   console.log('Что то пошло не так');
 } else if (budgetDay > 1200) {
   console.log('У вас высокий уровень дохода');
-} else if (budgetDay > 600 && budgetDay <= 1200) {
+} else if (budgetDay > 600 && budgetDay < 1200) {
   console.log('У вас средний уровень дохода');
-} else if (budgetDay <= 600) {
+} else if (budgetDay < 600 && budgetDay > 0) {
   console.log('К сожалению у вас уровень дохода ниже среднего');
+} else if (budgetDay === 1200) {
+  console.log('Ваш уровень дохода между средним и высоким');
+} else if (budgetDay === 600) {
+  console.log('Ваш уровень дохода между низким и средним');
 } else if (budgetDay === 0) {
   console.log('Вы не зарабатываете');
 }
