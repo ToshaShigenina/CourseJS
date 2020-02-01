621c0a77f9aeff042d3473adcd5d49f8307d22a0
Tosha Shigenina <shigenina@gmail.com>
2020-02-01 18:51:19 +0500

    Добавила вывод чисел с 2 и 4 в начале и вывод простых чисел

--- lesson05_add/script.js
+++ lesson05_add/script.js
@@ -1,1 +1,44 @@
+'use strict';
 
+let arr = [2458, 45896, 526, 58963, 45871, 5873, 457319];
+
+
+for (let i = 0; i < arr.length; i++) {
+  let a = arr[i].toString().split('');
+
+  if (a[0] === '2' || a[0] === '4') {
+    console.log(arr[i]);
+  }
+}
+
+
+let num = [];
+
+for (let i = 0; i < 100; i++) {
+  num[i] = true;
+}
+
+let p = 2;
+do {
+
+  for (let i = 2 * p; i < num.length; i += p) {
+    num[i] = false;
+  }
+
+  for (let i = p + 1; i < num.length; i++) {
+    if (num[i]) {
+      p = i;
+      break;
+    }
+  }
+
+} while (p * p < 100); // шаг 5
+
+for (let i = 1; i < num.length; i++) {
+  if (num[i]) {
+    console.log(i + ' - делители этого числа: 1 и  ' + i);
+  }
+}
+
+
+console.log(num);
