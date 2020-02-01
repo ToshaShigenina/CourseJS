b7fff6ab02b4695b2b243ded077fcf8061287900
Tosha Shigenina <shigenina@gmail.com>
2020-02-01 23:19:51 +0500

    Сделан перебор дней недели с помощью forEach

--- lesson07_add/script.js
+++ lesson07_add/script.js
@@ -1,1 +1,22 @@
+'use strict';
 
+let week = [
+  'Понедельник',
+  'Вторник',
+  'Среда',
+  'Четверг',
+  'Пятница',
+  'Суббота',
+  'Воскресенье'
+],
+  day = new Date().getDay();
+
+week.forEach((item, i) => {
+  if (i === day - 1) {
+    console.log('%c%s', 'font-weight: 700;', item);
+  } else if (i === 5 || i === 6) {
+    console.log('%c%s', 'font-style: italic;', item);
+  } else {
+    console.log(item);
+  }
+});
