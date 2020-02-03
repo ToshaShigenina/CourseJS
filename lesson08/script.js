59a1e191bce962c86f2ac5ea10fab71d6db5c463
Tosha Shigenina <shigenina@gmail.com>
2020-02-03 22:49:10 +0500

    Создана папка для 8го урока

---
+++ lesson08/script.js
@@ -0,0 +1,91 @@
+'use strict';
+
+let money,
+  isNumber = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+  },
+  start = function () {
+    do {
+      money = prompt('Ваш месячный доход?');
+    } while (!isNumber(money));
+  };
+
+start();
+
+let appData = {
+  budget: money,
+  budgetDay: 0,
+  budgetMonth: 0,
+  income: {},
+  addIncome: [],
+  expenses: {},
+  addExpenses: [],
+  expensesMonth: 0,
+  deposit: false,
+  mission: 50000,
+  period: 3,
+  asking: function () {
+    let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+      exp,
+      amount;
+
+    appData.addExpenses = addExpenses.toLowerCase().split(', ');
+    appData.deposit = confirm('Есть ли у вас депозит в банке?');
+
+    for (let i = 0; i < 2; i++) {
+      exp = prompt('Введите обязательную статью расходов?');
+
+      do {
+        amount = prompt('Во сколько это обойдется?');
+      } while (!isNumber(amount));
+
+      appData.expenses[exp] = +amount;
+    }
+  },
+  getExpensessMonth: function () {
+    let sum = 0;
+
+    for (let key in appData.expenses) {
+      sum += appData.expenses[key];
+    }
+
+    appData.expensesMonth = sum;
+  },
+  getBudget: function () {
+    appData.budgetMonth = appData.budget - appData.expensesMonth;
+    appData.budgetDay = Math.floor(appData.budgetMonth / 30);
+  },
+  getTargetMonth: function () {
+    appData.period = Math.ceil(appData.mission / appData.budgetMonth);
+
+    if (appData.period > 0) {
+      return 'Цель будет достигнута за ' + appData.period + ' месяцев';
+    } else {
+      return 'Цель не будет достигнута';
+    }
+  },
+  getStatusIncome: function () {
+    if (appData.budgetDay >= 1200) {
+      return 'У вас высокий уровень дохода';
+    } else if (appData.budgetDay >= 600 && appData.budgetDay < 1200) {
+      return 'У вас средний уровень дохода';
+    } else if (appData.budgetDay >= 0 && appData.budgetDay < 600) {
+      return 'К сожалению у вас уровень дохода ниже среднего';
+    } else {
+      return 'Что то пошло не так!';
+    }
+  }
+};
+
+appData.asking();
+appData.getExpensessMonth();
+appData.getBudget();
+
+console.log('Расходы за месяц: ' + appData.expensesMonth);
+console.log(appData.getTargetMonth());
+console.log(appData.getStatusIncome());
+console.log('Наша программа включает в себя данные:');
+
+for (let key in appData) {
+  console.log(key + ' : ' + appData[key]);
+}
